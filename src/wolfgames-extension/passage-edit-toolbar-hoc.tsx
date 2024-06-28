db1bb42a9b2997c2b016a65c47154b3d7df4c36a
Dmitry Lugovskoi <dmitry@wolfgames.ai>
2024-06-28 22:02:13 +0400

    [WF-1429] Trigger creation handling added

--- src/wolfgames-extension/passage-edit-toolbar-hoc.tsx
+++ src/wolfgames-extension/passage-edit-toolbar-hoc.tsx
@@ -257,6 +257,22 @@ export const withWolfgames = (StoryFormatToolbar: React.FC<StoryFormatToolbarPro
       }));
     }, [props.editor, messagingService, stories, dialogs]);
 
+    const createTriggerClickHandler = useCallback(() => {
+      if (!props.editor || !messagingService) {
+        return;
+      }
+
+      const cursorPosition = props.editor.indexFromPos(props.editor.getCursor());
+      const activePassageId = dialogs.at(0)?.props?.passageIds?.[0];
+
+      messagingService.send(new TwinejsCommandEditEvent({
+        command: TwineCustomCommand.Trigger,
+        initialValue: null,
+        startPosition: cursorPosition,
+        endPosition: cursorPosition,
+        currentPassageName: activePassageId && stories.at(0)?.passages.find(p => p.id === activePassageId)?.name,
+      }));
+    }, [props.editor, messagingService, stories, dialogs]);
 
     const createChatTriggerOffHandler = useCallback(() => {
       if (!props.editor || !messagingService) {
@@ -343,7 +359,7 @@ export const withWolfgames = (StoryFormatToolbar: React.FC<StoryFormatToolbarPro
             {
               disabled: props.disabled || !!selectedCommandsMap.size,
               label: 'Add Trigger',
-              onClick: () => {}
+              onClick: createTriggerClickHandler,
             },
             {
               disabled: props.disabled || !!selectedCommandsMap.size,
